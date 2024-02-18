520acaf8a7e39ecdaa5e5ce61b2c3192ff1dd388
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-02-18 19:49:56 +0100

    Update sensore_di_temperatura_ed_umidità.ino

--- sensore_di_temperatura_ed_umidità.ino
+++ sensore_di_temperatura_ed_umidità.ino
@@ -1,28 +1,29 @@
 #include <DHT.h>
-
 #include <LiquidCrystal.h>
-#include "DHT.h"
+
 #define DHTPIN 8
-// 8 è il pin di Arduino a cui collego il sensore di temperatura
+// Pin 8 on Arduino to which the temperature sensor is connected
 #define DHTTYPE DHT11
-// dht11 è il tipo di sensore che uso
+// DHT11 is the type of sensor being used
+
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-// connessione display (pin)
- 
+// Connection to the display (pins)
+
 void setup() {
-lcd.begin(16, 2);
-// imposto il tipo di display (colonne, righe)
-lcd.print("Temperatura");
-lcd.setCursor(0,1);
-lcd.print("Umidita'");
+  lcd.begin(16, 2);
+  // Set the display type (columns, rows)
+  lcd.print("Temperature");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity");
 }
+
 void loop() {
-int t = dht.readTemperature();
-int h = dht.readHumidity();
-// posiziono il cursore alla colonna 12 e riga 0
-lcd.setCursor(14, 0);
-lcd.print(t);
-lcd.setCursor(14, 1);
-lcd.print(h);
+  int t = dht.readTemperature();
+  int h = dht.readHumidity();
+  // Position the cursor at column 14 and row 0
+  lcd.setCursor(14, 0);
+  lcd.print(t);
+  lcd.setCursor(14, 1);
+  lcd.print(h);
 }
